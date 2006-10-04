8bde6e9c3d13904404dfd8967b38f9fcd8262c60
vipincherian <vipincherian@0f485fd7-ee1e-0410-9c02-f96baafee5d1>
2006-10-04 15:02:10 +0000

    Adding racommon.h
    git-svn-id: https://svn.code.sf.net/p/rosanne/code/trunk@20 0f485fd7-ee1e-0410-9c02-f96baafee5d1

---
+++ include/ra/racommon.h
@@ -0,0 +1,56 @@
+// rosanne : Twenty-Eight(28) Card Game
+// Copyright (C) 2006 Vipin Cherian
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin Street, Fifth Floor, 
+// Boston, MA  02110-1301, USA
+
+#ifndef _RACOMMON_H_
+#define _RACOMMON_H_
+
+#include "wx/wxprec.h"
+
+#ifdef __BORLANDC__
+#pragma hdrstop
+#endif
+
+#ifndef WX_PRECOMP
+#include "wx/wx.h"
+#endif
+
+#include "bh/bh.h"
+#include "ra/ralib.h"
+
+#define raTOTAL_CARDS 32
+#define raTOTAL_PLAYERS 4
+#define raTOTAL_BID_ROUNDS 3
+#define raPLAYER_INVALID -1
+#define raSUIT_INVALID -1
+#define raCARD_INVALID -1
+#define raPartner(X) ((X + 2) % raTOTAL_PLAYERS)
+#define raBID_INVALID -2
+#define raBID_PASS 0
+#define raBID_ALL -1
+#define raTOTAL_SUITS 4
+#define raTOTAL_VALUES 8
+#define raTOTAL_TRICKS 8
+#define raTOTAL_TEAMS 2 
+
+#define raGetSuit(X) (X / raTOTAL_VALUES)
+#define raGetValue(X) (X % raTOTAL_VALUES)
+#define raGetTeam(X) (X % raTOTAL_TEAMS)
+#define raGetOpponent(X) ((X + 1) % raTOTAL_TEAMS)
+
+#endif
+
