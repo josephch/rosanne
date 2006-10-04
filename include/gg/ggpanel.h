91b1369ce30a312ad8b88882eb9d1e5b8d20654e
vipincherian <vipincherian@0f485fd7-ee1e-0410-9c02-f96baafee5d1>
2006-10-04 14:49:41 +0000

    Adding file ggpanel.h
    git-svn-id: https://svn.code.sf.net/p/rosanne/code/trunk@16 0f485fd7-ee1e-0410-9c02-f96baafee5d1

---
+++ include/gg/ggpanel.h
@@ -0,0 +1,50 @@
+// rosanne : Twenty-Eight(28) Card Game
+// Copyright (C) 2006 Vipin Cherian
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin Street, Fifth Floor, 
+// Boston, MA  02110-1301, USA
+
+#ifndef _GGPANEL_H_
+#define _GGPANEL_H_
+
+#include "wx/wx.h"
+
+class ggPanel: public wxPanel
+{
+private:
+	wxBitmap *m_back, *m_work;
+	bool m_f_invalid;
+	wxRect m_rect_diff, m_rect_invalid;
+	DECLARE_EVENT_TABLE()
+public:
+	ggPanel(const wxWindow* parent);
+	~ggPanel();
+	bool Size();
+	bool Paint();
+	virtual void OnPaint(wxPaintEvent &event);
+	bool RefreshScreen();
+	bool BlitToBack(wxCoord xdest, wxCoord ydest, 
+		wxCoord width, wxCoord height, wxDC* source, wxCoord xsrc, 
+		wxCoord ysrc, int logicalFunc = wxCOPY, bool useMask = false, 
+		wxCoord xsrcMask = -1, wxCoord ysrcMask = -1);
+	bool BlitToFront(wxCoord xdest, wxCoord ydest, 
+		wxCoord width, wxCoord height, wxDC* source, wxCoord xsrc, 
+		wxCoord ysrc, int logicalFunc = wxCOPY, bool useMask = false, 
+		wxCoord xsrcMask = -1, wxCoord ysrcMask = -1);
+	bool ClearDifference();
+	//bool DrawBack();
+};
+
+#endif
