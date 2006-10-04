234dc470b4ab25de295253228426bddd58eb0ebb
vipincherian <vipincherian@0f485fd7-ee1e-0410-9c02-f96baafee5d1>
2006-10-04 15:22:46 +0000

    Adding raevents.cpp
    git-svn-id: https://svn.code.sf.net/p/rosanne/code/trunk@33 0f485fd7-ee1e-0410-9c02-f96baafee5d1

---
+++ src/ra/raevents.cpp
@@ -0,0 +1,68 @@
+// rosanne : Twenty-Eight(28) Card Game
+// Copyright (C) 2006 Vipin Cherian
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin Street, Fifth Floor, 
+// Boston, MA  02110-1301, USA
+
+#include "ra/raevents.h"
+
+const wxEventType raINFO_EVT = wxNewEventType();
+const wxEventType raBID_EVT = wxNewEventType();
+
+IMPLEMENT_DYNAMIC_CLASS(raInfoEvent, wxEvent)
+//DEFINE_EVENT_TYPE(EVT_GAME_NEW)
+
+raInfoEvent::raInfoEvent() : wxEvent( raINFO_EVT )
+{
+	// TODO : Use a #defined constant here. Include relevant header in this cpp
+	m_cmd = -1;
+}
+raInfoEvent::raInfoEvent(const raInfoEvent &evt)
+{
+	SetEventType(raINFO_EVT);
+	m_cmd = evt.m_cmd;
+}
+void raInfoEvent::SetCommand(int cmd)
+{
+	m_cmd = cmd;
+}
+int raInfoEvent::GetCommand()
+{
+	return m_cmd;
+}
+
+
+IMPLEMENT_DYNAMIC_CLASS(raBidEvent, wxEvent)
+//DEFINE_EVENT_TYPE(EVT_GAME_NEW)
+
+raBidEvent::raBidEvent() : wxEvent( raBID_EVT )
+{
+	// TODO : Use a #defined constant here. Include relevant header in this cpp
+	m_bid = -1;
+}
+raBidEvent::raBidEvent(const raBidEvent &evt)
+{
+	SetEventType(raBID_EVT);
+	m_bid = evt.m_bid;
+}
+void raBidEvent::SetBid(int bid)
+{
+	m_bid = bid;
+}
+int raBidEvent::GetBid()
+{
+	return m_bid;
+}
+
